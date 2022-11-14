641133d940f937087f0bb80e9f14faab78045eab
Vakurin <vakurin.maxim@gmail.com>
2022-11-14 12:27:10 +0200

    assets icons for website

---
+++ src/assets/index.tsx
@@ -0,0 +1,31 @@
+// mockups
+import daoLogoMock from "assets/mockups/basic-dao-logo.png";
+import NFTmock from "assets/mockups/basic-nft-image.png";
+
+// icons
+import backIcon from "assets/icons/back.png";
+import imageIcon from "assets/icons/image.png";
+import smartContractIcon from "assets/icons/smartContract.png";
+import viewAllIcon from "assets/icons/viewAll.png";
+
+import defaultToken from "assets/chains/emptyToken.webp";
+// tokens
+import Ethereum from "assets/chains/Ethereum.png";
+import Polygon from "assets/chains/Polygon.png";
+
+const ASSETS = {
+    // mockups
+    daoLogoMock: daoLogoMock,
+    NFTmock: NFTmock,
+    // icons
+    backIcon: backIcon,
+    imageIcon: imageIcon,
+    smartContractIcon: smartContractIcon,
+    viewAllIcon: viewAllIcon,
+    // tokens
+    defaultToken: defaultToken,
+    Ethereum: Ethereum,
+    Polygon: Polygon,
+};
+
+export default ASSETS;
