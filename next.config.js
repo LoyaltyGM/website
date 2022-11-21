8e4b34f3ce9bcd8056594e2234388b9922d5ef31
hokage229 <ernestzulpukarov@gmail.com>
2022-11-21 22:27:47 +0600

    change eslint config

--- next.config.js
+++ next.config.js
@@ -1,35 +1,31 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true"
+    enabled: process.env.ANALYZE === "true",
 });
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     i18n: {
         locales: ["en"],
-        defaultLocale: "en"
+        defaultLocale: "en",
     },
     images: {
-        domains: ["newconnection.infura-ipfs.io"]
+        domains: ["newconnection.infura-ipfs.io"],
     },
     eslint: {
-        ignoreDuringBuilds: true
+        ignoreDuringBuilds: true,
     },
     typescript: {
-        ignoreBuildErrors: true
-    }
+        ignoreBuildErrors: true,
+    },
+    redirects: () => {
+        return [
+            {
+                source: "/",
+                destination: "/waitlist",
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
-
-// next.config.js
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/waitlist',
-          permanent: true,
-        },
-      ];
-    },
-  };
