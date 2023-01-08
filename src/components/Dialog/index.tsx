d5e0e1c15bc57fc9765adf8576febacd46b6ca18
Vakurin <vakurin.maxim@gmail.com>
2023-01-08 21:43:34 +0400

    clean up project

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,5 +1,1 @@
 export * from "./base-dialogs";
-export * from "./ProposalVoteDialog";
-export * from "./CreateProposalDialogs";
-export * from "./CreateNftDialogs";
-export * from "./CreateDaoDialogs";
