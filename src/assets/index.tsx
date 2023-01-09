d74eb3922096fd6c41ed2b75879a6a1483959ef5
Vakurin <vakurin.maxim@gmail.com>
2023-01-09 14:49:50 +0400

    delete useless images

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,31 +1,29 @@
-// mockups
-import daoLogoMock from "assets/mockups/basic-dao-logo.png";
-import NFTmock from "assets/mockups/basic-nft-image.png";
+// Mockups
+import basicMockup from "assets/mockups/basic-image.png";
 
-// icons
+// Icons
 import imageIcon from "assets/icons/image.png";
-import defaultTokenIcon from "assets/chains/emptyToken.webp";
+import defaultTokenIcon from "assets/utils/emptyToken.webp";
 
-// video
+// Video/Gif
 import loyaltyGMgif_Original from "assets/gif/LGM_4mb.gif";
 
-// logo
+// Logo
 import suiEcosystemLogo from "assets/logo/SuiEcosystemLogo.png";
 import suietLogo from "assets/logo/SuietLogo.png";
 
-// lootboxes
+// Lootboxes
 import LBOX_S from "assets/lootbox/LBOX_S.png";
 import LQUESTION_S from "assets/lootbox/LQUESTION_S.png";
 
-// emoji
+// Emoji
 import emoji1 from "assets/emoji/Claps.png";
 import emoji2 from "assets/emoji/HandHoldingPencilLeft.png";
 import emoji3 from "assets/emoji/Victory.png";
 
 const ASSETS = {
     // mockups
-    daoLogoMock: daoLogoMock,
-    NFTmock: NFTmock,
+    basicMockup: basicMockup,
 
     // icons
     imageIcon: imageIcon,
